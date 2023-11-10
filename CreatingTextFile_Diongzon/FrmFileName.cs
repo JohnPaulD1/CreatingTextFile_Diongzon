794d4b97f039f2bd94a0a2ee165d7816673d5695
JohnPaulD1 <diongzon.226956@balagtas.sti.edu.ph>
2023-11-10 18:18:03 +0800

    Add project files.

---
+++ CreatingTextFile_Diongzon/FrmFileName.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CreatingTextFile_Diongzon
+{
+    public partial class FrmFileName : Form
+    {
+        public FrmFileName()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOkay_Click(object sender, EventArgs e)
+        {
+            static string SetFileName = txtFileName.Text;
+            Close();
+        }
+    }
+}
